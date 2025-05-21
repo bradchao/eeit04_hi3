f979293eacd91d5ad37e9331da9238bc9a33a616
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-21 14:33:49 +0800

    05211432

---
+++ h3/src/main/java/tw/brad/dao/MemberDao.java
@@ -0,0 +1,28 @@
+package tw.brad.dao;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import tw.brad.apis.BCrypt;
+import tw.brad.h3.HibernateUtil;
+import tw.brad.model.Member;
+
+public class MemberDao {
+	
+	public void save(Member member) {
+		Transaction transaction = null;
+		try(Session session = 
+				HibernateUtil.getSessionFactory().openSession()){
+			transaction = session.beginTransaction();
+			
+			member.setPasswd(
+				BCrypt.hashpw(member.getPasswd(), BCrypt.gensalt()));
+			
+			session.persist(member);
+			
+			transaction.commit();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+}
---
+++ h3/src/main/java/tw/brad/h3/Brad05.java
@@ -0,0 +1,17 @@
+package tw.brad.h3;
+
+import tw.brad.dao.MemberDao;
+import tw.brad.model.Member;
+
+public class Brad05 {
+	public static void main(String[] args) {
+		Member member = new Member();
+		member.setAccount("mark");
+		member.setPasswd("123456");
+		member.setCname("Mark");
+		
+		MemberDao dao = new MemberDao();
+		dao.save(member);
+		
+	}
+}
