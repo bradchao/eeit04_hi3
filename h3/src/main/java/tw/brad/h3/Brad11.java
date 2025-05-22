bc8451896163b9debb35ac679956847bf3b5fed3
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-22 11:57:37 +0800

    05221157

---
+++ h3/src/main/java/tw/brad/h3/Brad11.java
@@ -0,0 +1,31 @@
+package tw.brad.h3;
+
+import java.util.Date;
+
+import tw.brad.dao.CustDao;
+import tw.brad.model.Cust;
+import tw.brad.model.Order;
+
+public class Brad11 {
+
+	public static void main(String[] args) {
+		CustDao custDao = new CustDao();
+		Cust cust = custDao.getById(1);
+		if (cust != null) {
+			//System.out.println(cust.getOrders().size());
+			Order order1 = new Order();
+			order1.setOrderDate(new Date());
+			
+			Order order2 = new Order();
+			order2.setOrderDate(new Date());
+			
+			cust.addOrder(order1);
+			cust.addOrder(order2);
+			
+			custDao.update(cust);
+		}else {
+			System.out.println("NOT FOUND");
+		}
+	}
+
+}
